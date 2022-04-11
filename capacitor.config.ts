add0afab410b38f5038e352658b66f8a71aad41b
Flavius <Flavius@DESKTOP-EHJMCEG>
2022-04-11 20:24:13 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'noteApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
